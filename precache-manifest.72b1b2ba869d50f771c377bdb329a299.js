c35cdcfdc2a2cd2c2117f95186281654023f06dc
Kevin Wang <gumtree200@gmail.com>
2020-07-06 21:13:12 +1000

    Updates

---
+++ precache-manifest.72b1b2ba869d50f771c377bdb329a299.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d137de9752edd4a230d9e5609145514",
+    "url": "/veryrich/index.html"
+  },
+  {
+    "revision": "df1d2dee2f0b9a6a9a80",
+    "url": "/veryrich/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "fad41f2c0158823f4257",
+    "url": "/veryrich/static/css/main.5db5b2d9.chunk.css"
+  },
+  {
+    "revision": "df1d2dee2f0b9a6a9a80",
+    "url": "/veryrich/static/js/2.ecf0f34c.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/veryrich/static/js/2.ecf0f34c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fad41f2c0158823f4257",
+    "url": "/veryrich/static/js/main.a6faf5f3.chunk.js"
+  },
+  {
+    "revision": "c7ee737114c3729bbce1",
+    "url": "/veryrich/static/js/runtime-main.52ffc21d.js"
+  }
+]);
